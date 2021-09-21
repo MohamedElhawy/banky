d00e03b8dfb362ef3ea674300e16188c882020de
Mohamed Elhawy <engineermohamedelhawy@gmail.com>
2021-09-21 17:10:45 +0200

    finished web app  "Banky" (desktop version)

--- js/script.js
+++ js/script.js
@@ -24,11 +24,8 @@ const loadAmount = document.getElementById("loadAmount");
 const loanBtn = document.getElementById("loanBtn");
 const sortBtn = document.getElementById("sortBtn");
 const sortArrow = document.getElementById("sortArrow");
-
-
-// Global variables
-let activeAccount;
-let sortIndex = true;
+const currentBalanceDate = document.getElementById("currentBalanceDate");
+const logOutTimer = document.getElementById("logOutTimer");
 
 
 
@@ -38,7 +35,19 @@ const account1 =
     owner : "Mohamed Elhawy",
     Movement : [200 , 450 , -400 , 3000 , -650 , -130 , 70 , 1300],
     interestRate : 1.2,
-    pin : 1111
+    pin : 1111,
+    movementsDates : [
+        '2021-01-18T21:31:19.178Z',
+        '2021-02-23T07:31:37.178Z',
+        '2021-03-28T09:31:17.178Z',
+        '2021-04-01T10:31:12.178Z',
+        '2021-05-08T14:31:17.178Z',
+        '2021-09-16T17:31:57.178Z',
+        '2021-09-20T08:31:40.178Z',
+        '2021-09-21T10:31:08.178Z'
+    ],
+    local: 'en-US',
+    currency: "USD"
 };
 
 const account2 = 
@@ -46,7 +55,19 @@ const account2 =
     owner : "Sara Elhawy",
     Movement : [5000 , 3400 , -150 , -790 , -3210 , -1000 , 8500 , -30],
     interestRate : 1.5,
-    pin : 2222
+    pin : 2222,
+    movementsDates : [
+        '2019-11-01T13:15:33.178Z',
+        '2019-11-30T09:48:12.448Z',
+        '2019-12-25T06:04:17.178Z',
+        '2020-01-25T14:31:16.178Z',
+        '2020-02-05T16:34:35.178Z',
+        '2020-03-10T14:22:17.178Z',
+        '2020-06-25T18:54:43.178Z',
+        '2020-07-26T12:43:45.178Z'
+    ],
+    local: 'ar-BH',
+    currency: "BHD"
 };
 
 const account3 = 
@@ -54,7 +75,19 @@ const account3 =
     owner : "Ahmed Elhawy",
     Movement : [200 , -200 , 340 , -300 , -20 , 50 , 400 , -460],
     interestRate : 0.7,
-    pin : 3333
+    pin : 3333,
+    movementsDates : [
+        '2019-11-18T21:31:17.178Z',
+        '2019-12-23T07:31:17.178Z',
+        '2020-01-28T09:31:17.178Z',
+        '2020-04-01T10:31:17.178Z',
+        '2020-05-08T14:31:17.178Z',
+        '2020-05-27T17:31:17.178Z',
+        '2020-07-11T23:31:17.178Z',
+        '2020-07-12T10:31:17.178Z'
+    ],
+    local: 'ru-RU',
+    currency: "RUB"
 };
 
 const account4 = 
@@ -62,7 +95,19 @@ const account4 =
     owner : "naruto uzumaki",
     Movement : [430 , 1000 , 700 , 50 , 90],
     interestRate : 1,
-    pin : 4444
+    pin : 4444,
+    movementsDates : [
+        '2019-11-18T21:31:17.178Z',
+        '2019-12-23T07:31:17.178Z',
+        '2020-01-28T09:31:17.178Z',
+        '2020-04-01T10:31:17.178Z',
+        '2020-05-08T14:31:17.178Z',
+        '2020-05-27T17:31:17.178Z',
+        '2020-07-11T23:31:17.178Z',
+        '2020-07-12T10:31:17.178Z'
+    ],
+    local: 'ja-JP',
+    currency: "JPY"
 };
 
 const accounts = [account1, account2, account3, account4];  
@@ -75,16 +120,64 @@ const accounts = [account1, account2, account3, account4];
 
 
 
+// Global variables
+let activeAccount;
+let sortIndex = true;
+let CurrentDate = new Date(Date.now());
+let timer;
+
+
+
+
+
+
+// localizing the time
+function localizeTime (account , date)
+{
+    let local = account.local;
+    let localOption = {
+        hour: "numeric",
+        minute: "numeric",
+        day: "numeric",
+        month: "numeric",
+        year: "numeric"
+    };
+
+    let IntlCurrentDate = new Intl.DateTimeFormat(local , localOption ).format(date);
+    return IntlCurrentDate;
+}
+
+
+// localize the currency
+function localizeCurrency (account , number)
+{
+    let currency = account.currency;
+
+    let moneyOption = {
+        style: "currency",
+        currency: account.currency
+    };
+
+    let localizeMoney = new Intl.NumberFormat(account.local , moneyOption).format(number);
+
+    return localizeMoney;
+}
+
+
+
+
 // movements (transaction log) inserting
 
-function insertingLogData (movements , sort=false)
+function insertingLogData (account , sort=false)
 {
 
     // clean the container first
     transactionLog.innerHTML = "";
 
-    let movs = [...movements];
-    // sorting direction
+
+    // sorting 
+    let movs = [...account.Movement];
+
     if ( sort )
     {
         movs = movs.sort( (a , b) => a - b);
@@ -92,15 +185,57 @@ function insertingLogData (movements , sort=false)
     }
     else
     {
-        movs = movements;
+        movs = account.Movement;
         sortIndex = true;
     }
 
 
+
+    // inserting logs
     for ( let i = 0 ; i < movs.length ; i++)
     {
+
         // to control the color of the state
         let checkStatus = movs[i] > 0 ? "" : "withdrawal";
+
+
+        // setting date
+        let date = new Date(account.movementsDates[i]);
+
+        let formatedLogDate = localizeTime(account , date) ;
+
+
+        
+        // display offset of the dates
+        let todayDateInMS = Number(CurrentDate);
+        let logDateInMS = Number(date);
+
+        let timeDifferenceInDays = Math.trunc( (todayDateInMS - logDateInMS) / (1000 * 60 * 60 * 24)  ) ;
+        // console.log(timeDifferenceInDays);
+        
+        let offsetDays;
+
+        if ( timeDifferenceInDays === 0 )
+        {
+            offsetDays = "TODAY";
+        }
+        else if ( timeDifferenceInDays === 1 )
+        {
+            offsetDays = "YESTERDAY";
+        }
+        else if ( timeDifferenceInDays <= 7 )
+        {
+            offsetDays = `${timeDifferenceInDays} Days ago`;
+        }
+        else
+        {
+            offsetDays = "";
+        }
+
+
+        // localize money :D
+        let moneyValurToLocal = localizeCurrency( account , movs[i]);
+
 
         // log element
         const logEl = 
@@ -108,14 +243,33 @@ function insertingLogData (movements , sort=false)
         <div class="transactionLog-entry">
             <div class="entry-head">
                 <div class="entry-states ${checkStatus}">${i+1} ${movs[i] > 0 ? "DEPOSIT" : "WITHDRAWAL"}</div>
-                <div class="entry-date">TODAY</div>
+                <div class="entry-date">${formatedLogDate}</div>
+                <span class="entry-offset">${offsetDays}<span>
             </div>
-            <div class="entry-amount">${movs[i] + "€"}</div>
+            <div class="entry-amount">${moneyValurToLocal}</div>
         </div>
         `;
 
         // insert log elements to DOM
         transactionLog.insertAdjacentHTML("afterbegin" , logEl);
+
+
+        const entryOffset = document.querySelector(".entry-offset");
+
+        if ( timeDifferenceInDays === 0 )
+        {
+            entryOffset.classList.add("offset-visible");
+        }
+        else if ( timeDifferenceInDays === 1 )
+        {
+            entryOffset.classList.add("offset-visible");
+        }
+        else if ( timeDifferenceInDays <= 7 )
+        {
+            entryOffset.classList.add("offset-visible");
+        }
+      
+
 
     }
 
@@ -147,6 +301,9 @@ createUserNameProp(accounts);
 
 
 
+
+
+
 // calculate balance
 
 function calculateBalance (account)
@@ -156,27 +313,27 @@ function calculateBalance (account)
 
     activeAccount.balance = totalBalance;
 
-    balanceValue.textContent = totalBalance + "€";
+    balanceValue.textContent = localizeCurrency(account , totalBalance);
 }
 
 
 
 // calculate transaction status
 
-function calculateTransInOutInterest (trans)
+function calculateTransInOutInterest (account)
 {
     // in
-    let transInput = trans.Movement.filter( el => el > 0 ).reduce( (acc,el) => acc + el , 0);
-    transIn.textContent = transInput + "€";
+    let transInput = account.Movement.filter( el => el > 0 ).reduce( (acc,el) => acc + el , 0);
+    transIn.textContent = localizeCurrency(account , transInput);
 
     // out
-    let transOutput = trans.Movement.filter( el => el < 0 ).reduce( (acc,el) => acc + el , 0);
+    let transOutput = account.Movement.filter( el => el < 0 ).reduce( (acc,el) => acc + el , 0);
     transOutput = Math.abs(transOutput);
-    transOut.textContent = transOutput + "€";
+    transOut.textContent = localizeCurrency(account , transOutput);
 
     // interest
-    let transInt = trans.Movement.filter( el => el > 0 ).map( el => (el * trans.interestRate) / 100 ).filter( el => el >= 1 ).reduce( (acc,el) => acc + el , 0);
-    transInterest.textContent = transInt + "€";
+    let transInt = account.Movement.filter( el => el > 0 ).map( el => (el * account.interestRate) / 100 ).filter( el => el >= 1 ).reduce( (acc,el) => acc + el , 0);
+    transInterest.textContent = localizeCurrency(account , transInt);
 
 }
 
@@ -214,6 +371,18 @@ function signingIn (e)
         // change the header text to a welcoming text for specific user
 
         headerText.textContent = "Welcome back, " + activeAccount.owner.split(" ")[0];
+
+        // localize time
+        currentBalanceDate.textContent = localizeTime(activeAccount , CurrentDate);
+
+
+
+        // start logging out timer
+
+        // check to prevent a previews timer still running
+        if (timer) clearInterval(timer);
+        // start timer
+        timer = timerLoggingOut();
 
 
         // update UI
@@ -264,6 +433,8 @@ function transferingMoney (e)
     {
         activeAccount.Movement.push(-transmittedAmount);
         recieverObject.Movement.push(transmittedAmount);
+        activeAccount.movementsDates.push(new Date().toISOString());
+        recieverObject.movementsDates.push(new Date().toISOString());
         transferTo.value = "";
         transferAmount.value = "";
     }
@@ -275,26 +446,16 @@ function transferingMoney (e)
     // update UI
     updateUI(activeAccount);
 
+    // reset timer
+    clearInterval(timer);
+    timer = timerLoggingOut();
+
 }
 
 
 transferBtn.addEventListener("click" , transferingMoney);
 
 
-
-
-
-
-// update UI
-
-function updateUI (account)
-{
-    insertingLogData(account.Movement);
-
-    calculateBalance(account);
-
-    calculateTransInOutInterest(account);
-}
 
 
 
@@ -341,8 +502,7 @@ confirmBtn.addEventListener("click" , closingAccount );
 
 
 
-
-// getting a load
+// getting a loan
 
 function requistingLoad (e)
 {
@@ -350,12 +510,22 @@ function requistingLoad (e)
 
     let amount = Number(loadAmount.value);
 
+    // validate input (amount)
     if ( amount > 0  && activeAccount.Movement.some( el => el >= amount * 0.1 ) )
     {
-        activeAccount.Movement.push(amount);
+        setTimeout( function ()
+        {
 
-        // update UI
-        updateUI(activeAccount);
+            activeAccount.Movement.push(amount);
+
+            activeAccount.movementsDates.push(new Date().toISOString());
+    
+            // update UI
+            updateUI(activeAccount);
+
+
+        } , 2500);
+
 
         // clean
         loadAmount.value = "";
@@ -364,6 +534,10 @@ function requistingLoad (e)
     {
         alert("☹ Unfortionatly, we cannot loan you the amount requisted, since you didn't deposite enough income. ☹");
     }
+
+    // reset timer
+    clearInterval(timer);
+    timer = timerLoggingOut();
 
 
 }
@@ -376,10 +550,24 @@ loanBtn.addEventListener("click" , requistingLoad);
 
 
 
+// update UI
+
+function updateUI (account)
+{
+    insertingLogData(account);
+
+    calculateBalance(account);
+
+    calculateTransInOutInterest(account);
+
+}
+
+
+
 // sort button
 function sortingLog ()
 {
-    insertingLogData(activeAccount.Movement , sortIndex);
+    insertingLogData(activeAccount , sortIndex);
     if ( !sortArrow.classList.contains("sortRotate") )
     {
         sortArrow.classList.add("sortRotate");
@@ -392,103 +580,39 @@ function sortingLog ()
 }
 
 
-
 sortBtn.addEventListener ("click" , sortingLog);
 
 
 
-// test
-
-const dogs = [
-    {weight: 22 , curFood: 250,owners: ['Alice' , 'Bob']},
-    {weight: 8 , curFood: 200,owners: ['Matilda']},
-    {weight: 13 , curFood: 275,owners: ['Sarah' , 'John']},
-    {weight: 32 , curFood: 340,owners: ['Michael']}
-];
-
-
-// 1
-
-dogs.forEach(
-
-    function (el)
-    {
-        el.recommendedFood = el.weight ** 0.75 * 28;
-    }
-
-);
-
-console.log(dogs);
-
-
-
-// 2
-
-let sarahDog = dogs.find( 
-    function (e)
-    {
-        return e.owners.includes("Sarah");
-    }
-);
-
-if ( sarahDog.recommendedFood < (sarahDog.curFood) )
+// logging user out timer
+function timerLoggingOut ()
 {
-    console.log("Too Much");
+
+    let time = 5 * 60;  // seconds (5 minutes in seconds)
+
+    let tick = function ()
+    {
+
+        let timeMinute = String(Math.trunc(time / 60)).padStart(2,0);
+        let timeSecond = String(time % 60).padStart(2,0);
+
+        logOutTimer.textContent = `${timeMinute}:${timeSecond}`;
+
+
+        if ( time === 0 )
+        {
+            clearInterval(timer);
+            headerText.textContent = "Log in to get started";
+            main.style.opacity = "0";
+        }
+
+
+        time--;
+    };
+
+
+    let timer = setInterval(tick , 1000);
+
+    return timer;
+
 }
-else if ( sarahDog.recommendedFood > (sarahDog.curFood) )
-{
-    console.log("Too little");
-}
-
-
-
-// 3
-
-let ownersEatTooMuch = [];
-let ownersEatTooLittle = [];
-
-let dogsEatTooMuch = dogs.filter( (el) => { return el.curFood > el.recommendedFood } );
-let dogsEatTooLittle = dogs.filter( (el) => { return el.curFood < el.recommendedFood } );
-
-
-ownersEatTooMuch = dogsEatTooMuch.map( (el) => { return el.owners} ).flat();
-ownersEatTooLittle = dogsEatTooLittle.map( (el) => { return el.owners} ).flat();
-
-console.log(ownersEatTooMuch);
-console.log(ownersEatTooLittle);
-
-// 4
-
-let stringOwnserEatTooMuch = ownersEatTooMuch.join(" and ");
-console.log(`${stringOwnserEatTooMuch}'s dogs eat too much!`);
-
-let stringOwnserEatTooLittle = ownersEatTooLittle.join(" and ");
-console.log(`${stringOwnserEatTooLittle}'s dogs eat too Little!`);
-
-
-// 5
-
-
-let dogOkayAmount = dogs.some( (dog) => {return dog.curFood === dog.recommendedFood } );
-
-console.log(dogOkayAmount);
-
-
-
-// 6
-
-let dogExactAmount = dogs.some( (dog) => {return dog.curFood >= (dog.recommendedFood * 0.9) && dog.curFood <= (dog.recommendedFood * 1.1) } );
-
-console.log(dogExactAmount);
-
-
-
-// 7
-
-let dogsEatingOkayAmount = dogs.filter( (dog) => { return ( dog.curFood >= (dog.recommendedFood * 0.9) && dog.curFood <= (dog.recommendedFood * 1.1) )  } );
-console.log(dogsEatingOkayAmount);
-
-// 8
-
-let dogs2_0 = [...dogs].sort( (a , b) => {return a.recommendedFood - b.recommendedFood } );
-console.log(dogs2_0);
